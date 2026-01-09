7ab9d99444cf2d5b183284e10eb2385ea4c3a41e
yRibot <yoanribot@gmail.com>
2026-01-09 23:06:33 +0100

    Update API_BASE_URL to use environment variable for Strapi configuration

--- landing/lib/constants.ts
+++ landing/lib/constants.ts
@@ -1,3 +1,4 @@
-export const API_BASE_URL = "http://localhost:1337";
+export const API_BASE_URL =
+  process.env.STRAPI_BASE_URL || "http://localhost:1337";
 const HOMEPAGE_BASE_URL = `${API_BASE_URL}/api/home-page`;
 export const HERO_BANNER_DATA_PATH = `${HOMEPAGE_BASE_URL}?populate[sections][on][layout.hero-section][populate][image][fields][0]&populate[sections][on][layout.hero-section][populate][link][populate]`;
