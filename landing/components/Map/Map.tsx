6e97fdb50d809d4f60b0bf90ba5cd74a26e42321
yRibot <yoanribot@gmail.com>
2026-02-14 13:15:21 +0100

    Update Map component styles and adjust Card module for consistent width

--- landing/components/Map/Map.tsx
+++ landing/components/Map/Map.tsx
@@ -10,7 +10,7 @@ const Map = ({ title, phoneMain, address, schedules }: MapSectionProps) => {
     <section className={sharedStyles.container}>
       <SectionTitle title={title} />
 
-      <section className="flex flex-col md:flex-row gap-12">
+      <section className="flex flex-col md:flex-row gap-12 mb-10">
         <div className={styles.mapContainer}>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1518.1917175293722!2d-3.6161087017516556!3d40.44464964424175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd422fa5bf86a4dd%3A0x28c23634ca08de3f!2sC.%20Alcal%C3%A1%2C%20573%2C%20San%20Blas-Canillejas%2C%2028022%20Madrid%2C%20Spain!5e0!3m2!1sen!2sfr!4v1771016881177!5m2!1sen!2sfr"
