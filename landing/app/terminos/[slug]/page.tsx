826d3f8431db6e0a9e801f3cd516897be969a7a8
yRibot <yoanribot@gmail.com>
2026-02-26 13:44:26 +0100

    refactor: simplify blog filtering logic in generateStaticParams function

--- landing/app/terminos/[slug]/page.tsx
+++ landing/app/terminos/[slug]/page.tsx
@@ -37,12 +37,10 @@ export default async function Page({ params }: DynamicPageProps) {
 
 export async function generateStaticParams() {
   const blogs = await getBlogs();
-  const filteredBlogs = Array.isArray(blogs)
-    ? blogs.filter(
-        (blog): blog is BlogData & { slug: string } =>
-          Boolean((blog as { slug?: string } | null)?.slug),
-      )
-    : [];
+  const filteredBlogs =
+    blogs.filter((blog): blog is BlogData & { slug: string } =>
+      Boolean((blog as { slug?: string } | null)?.slug),
+    ) ?? [];
 
   return filteredBlogs.map((blog) => ({
     slug: blog.slug,
